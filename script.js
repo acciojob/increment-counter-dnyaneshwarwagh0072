123b8bc417dbc77c02d9e1913e3c7bb4755cd293
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-12-03 19:02:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const counterElement = document.getElementById('counter');
+const incrementButton = document.getElementById('incrementBtn');
+
+let counterValue = 0;
+incrementButton.addEventListener('click', () => {
+    alert(`${counterValue}`);
+    counterValue++;
+    counterElement.textContent = counterValue;
+});
